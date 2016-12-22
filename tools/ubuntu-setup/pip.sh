c84538cf93602f7971261a35e2ca6a8fef342fe5
Li Yi <weiyuan.yl@alibaba-inc.com>
2016-12-22 13:39:42 +0800

    Add mirror sites
    Add local mirrors

--- tools/ubuntu-setup/pip.sh
+++ tools/ubuntu-setup/pip.sh
@@ -1,3 +1,11 @@
 sudo apt-get install -y python-pip
+mkdir -p ~/.pip
+cat > ~/.pip/pip.conf <<EOF
+[global]
+index-url = http://mirrors.aliyun.com/pypi/simple/
+
+[install]
+trusted-host=mirrors.aliyun.com
+EOF
 sudo pip install jsonschema
 sudo pip install argcomplete
--- tools/ubuntu-setup/scala.sh
+++ tools/ubuntu-setup/scala.sh
@@ -1,4 +1,5 @@
-wget www.scala-lang.org/files/archive/scala-2.11.6.deb -O /tmp/scala-2.11.6.deb
+#wget www.scala-lang.org/files/archive/scala-2.11.6.deb -O /tmp/scala-2.11.6.deb
+wget http://acs-upload.oss-cn-qingdao.aliyuncs.com/scala-2.11.6.deb -O /tmp/scala-2.11.6.deb
 sudo dpkg -i /tmp/scala-2.11.6.deb
 sudo apt-get update
 sudo apt-get install -y scala
